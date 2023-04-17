90ce93954c59fd75b3ed73add2ec1345e498d089
eityans <eityans@gmail.com>
2023-04-17 15:44:32 +0900

    annotate

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tickets
+#
+#  id         :bigint           not null, primary key
+#  code       :string(10)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_tickets_on_code  (code) UNIQUE
+#
 class Ticket < ApplicationRecord
-  validates :code, presence: true, uniqueness: true, length: { maximum: 10 }
+  validates :code, presence: true, uniqueness: true
 end
